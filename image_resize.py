2e93296245b70236167b916319e4f15deb416832
cr00z <cruz@list.ru>
2019-04-10 23:57:22 +0700

    create simple example script

--- image_resize.py
+++ image_resize.py
@@ -1,8 +1,12 @@
+from PIL import Image
 
 
 def resize_image(path_to_original, path_to_result):
+    image = Image.open('photo.jpg')
+    resized_image = image.resize((500, 500))
+    resized_image.save('photo_500x500.jpg')
     pass
 
 
 if __name__ == '__main__':
-    pass
+    resize_image('','')
