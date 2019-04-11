4b2b86d3ec2205ed574069cb9c20715b17a210e3
cr00z <cruz@list.ru>
2019-04-11 09:21:04 +0700

    add argparse

--- image_resize.py
+++ image_resize.py
@@ -1,4 +1,5 @@
 from PIL import Image
+import argparse
 
 
 def resize_image(path_to_original, path_to_result):
@@ -8,5 +9,36 @@ def resize_image(path_to_original, path_to_result):
     pass
 
 
+def get_cmdline_args():
+    parser = argparse.ArgumentParser(
+        description='Simple console script for resize JPG and PNG images'
+    )
+    parser.add_argument(
+        'input_path',
+        metavar = "INPUT",
+        help='path for input image')
+    parser.add_argument(
+        '--width',
+        type=int,
+        help='new width of image'
+    )
+    parser.add_argument(
+        '--height',
+        type=int,
+        help='new height of image'
+    )
+    parser.add_argument(
+        '--scale',
+        type=float,
+        help='new scale of image'
+    )
+    parser.add_argument(
+        '--output',
+        help='path for output image'
+    )
+    return parser.parse_args()
+
+
 if __name__ == '__main__':
+    args = get_cmdline_args()
     resize_image('','')
